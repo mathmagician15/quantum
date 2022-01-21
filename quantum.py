3f848ca611bab6b8c45140e1802ccb3564cbb5a9
Matthew Vine <matt@Matthews-MacBook-Pro.local>
2022-01-21 16:43:43 -0500

    initial commit part 2

--- quantum.py
+++ quantum.py
@@ -1,6 +1,18 @@
-import qiskit
-import numpy
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator
+import numpy as np
 
+qc = QuantumCircuit(2)
 
-def thebeginningoftheend()
-    
+qc.h(0)
+
+qc.cx(0,1)
+
+qc.measure_all()
+
+qc.draw()
+
+#U = Operator(qc)
+
+# Show the results
+#U.data
